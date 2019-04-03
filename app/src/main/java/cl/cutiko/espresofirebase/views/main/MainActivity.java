d4ced7ce94ff3a0a0f31ada7bfd25d253e5cb6b5
cutiko <cutiko@gmail.com>
2019-04-03 11:49:21 -0300

    ADD: error case for contract

--- app/src/main/java/cl/cutiko/espresofirebase/views/main/MainActivity.java
+++ app/src/main/java/cl/cutiko/espresofirebase/views/main/MainActivity.java
@@ -8,17 +8,24 @@ import cl.cutiko.espresofirebase.R;
 
 public class MainActivity extends AppCompatActivity implements MainContract.View {
 
+    private TextView tv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = findViewById(R.id.taskCountTv);
         new Presenter(this).getUserTasks();
     }
 
     @Override
     public void setTasksNumber(int count) {
-        TextView tv = findViewById(R.id.taskCountTv);
         String text = getResources().getQuantityString(R.plurals.tasks_plurals, count, count);
         tv.setText(text);
+    }
+
+    @Override
+    public void error() {
+        tv.setText(R.string.error);
     }
 }
--- app/src/main/java/cl/cutiko/espresofirebase/views/main/MainContract.java
+++ app/src/main/java/cl/cutiko/espresofirebase/views/main/MainContract.java
@@ -4,6 +4,7 @@ public interface MainContract {
 
     interface View {
         void setTasksNumber(int count);
+        void error();
     }
 
     interface Presenter {
